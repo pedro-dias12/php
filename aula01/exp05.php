54bd18b752e4eb65adc05c0faef2321d398bdcfa
Pedro Henrique Cardoso Dias <145489366+pedro-dias12@users.noreply.github.com>
2023-10-17 16:25:19 -0300

    Add files via upload

---
+++ aula01/exp05.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $curso = "web médio - aluno 10!";
+    $disciplina = "PSW1";
+    $turma = 2022;
+    echo "[curso] : $curso <br>";
+    echo "[disciplina] ; $disciplina <br>";
+    echo "[turma] : $turma <br>";
+
+    echo "sua " , "mae" , " d4";
+    
+    ?>
+
+
+
+</body>
+</html>
