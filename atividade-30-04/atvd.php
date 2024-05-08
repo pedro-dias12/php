6ae0046dc3d5200706994b80690611ba60a0c590
Pedro Henrique Cardoso Dias <145489366+pedro-dias12@users.noreply.github.com>
2024-05-08 08:47:55 -0300

    Add files via upload

---
+++ atividade-30-04/atvd.php
@@ -0,0 +1,87 @@
+<?php
+$aluno1 ["nome"] = "bruna"
+
+$aluno1 ["endereco"] = "rua da mata"
+
+$aluno1 ["cidade"] = "ibieassuce"
+
+$aluno1 ["estado"] = "bahia"
+
+$aluno1 ["idade"] = "20"
+
+$aluno1 ["cpf"] = "197.567.170-07"
+?>
+
+<?php
+$aluno2 ["nome"] = "yasmin"
+
+$aluno2 ["endereco"] = "montes claros"
+
+$aluno2 ["cidade"] = "Espinosa"
+
+$aluno2 ["estado"] = "Minas"
+
+$aluno2 ["idade"] = "21"
+
+$aluno2 ["cpf"] = "187.568.173-07"
+?>
+
+<?php
+$aluno3 ["nome"] = "Felipe"
+
+$aluno3 ["endereco"] = "Avenida Santos Dortmund"
+
+$aluno3 ["cidade"] = "Guanambi"
+
+$aluno3 ["estado"] = "bahia"
+
+$aluno3 ["idade"] = "18"
+
+$aluno3 ["cpf"] = "237.127.153-05"
+?>
+
+<?php
+$professor1 ["nome"] = "Pedro Dias"
+
+$professor1  ["endereco"] = "Rua Herminio Silveira"
+
+$professor1 ["cidade"] = "Espinosa"
+
+$professor1 ["estado"] = "Minas"
+
+$professor1 ["idade"] = "17"
+
+$professor1 ["cpf"] = "112.179.170-07"
+?>
+
+<?php
+$professor2  ["nome"] = "Queila"
+
+$professor2 ["endereco"] = "Rua fogoso"
+
+$professor2 ["cidade"] = "guanambi"
+
+$professor2 ["estado"] = "bahia"
+
+$professor2 ["idade"] = "30"
+
+$professor2 ["cpf"] = "178.567.170-12"
+?>
+
+<?php
+$professor3 ["nome"] = "Thales"
+
+$professor3 ["endereco"] = "rua Tiradantes"
+
+$professor3 ["cidade"] = "Guanambi"
+
+$professor3 ["estado"] = "bahia"
+
+$professor3 ["idade"] = "25"
+
+$professor3 ["cpf"] = "165.232.324-07"
+?>
+
+
+
+
